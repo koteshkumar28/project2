4834d92bd0b86e4edb487871d62b840f2d3e251b
koteshkumar28 <koteshkumar28@gmail.com>
2022-05-02 15:28:55 +0530

    Added project to git

---
+++ src/core_java/Koti.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class Koti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		int c=a+b;
+		System.out.println("sum of two number : "+c);
+	}
+
+}
